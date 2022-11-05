13f923a2bcec2ec6e5229065ad89439df713420a
Jihed Ounis <tariqaq123@gmail.com>
2022-11-05 12:11:00 +0100

    first

---
+++ src/components/carousel/data.js
@@ -0,0 +1,17 @@
+import DragonBall from "https://c4.wallpaperflare.com/wallpaper/109/220/12/dragon-ball-dragon-ball-super-ultra-instinct-son-goku-wallpaper-preview.jpg";
+import BlueLock from "https://images4.alphacoders.com/116/thumb-1920-1165712.jpg";
+import Berserk from "https://images.hdqwalls.com/wallpapers/berserk-aj.jpg";
+
+export const images = [
+  { title: "San Diego", subtitle: "This is San Diego", img: DragonBall },
+  {
+    title: "Salvador, Brazil",
+    subtitle: "The Best City in the World",
+    img: BlueLock,
+  },
+  {
+    title: "UBC (Vancouver)",
+    subtitle: "The University of British Columbia",
+    img: Berserk,
+  },
+];
